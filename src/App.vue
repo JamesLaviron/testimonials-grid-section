7f88c26d62baa58ba77647b5754668a7b3e749d5
JamesLaviron <victor@gerfaud.fr>
2024-09-05 21:39:17 +0200

    Raw html

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,129 @@
 <template>
-  <main></main>
+  <main>
+    <article class="card">
+      <header class="card__header">
+        <h1 class="card__title">Daniel Clifford</h1>
+        <p class="card__graduation">Verified Graduate</p>
+      </header>
+
+      <p class="card__description">
+        I received a job offer mid-course, and the subjects I learned were
+        current, if not more so, in the company I joined. I honestly feel I got
+        every penny’s worth.
+      </p>
+
+      <blockquote
+        class="card__quote"
+        cite="https://example.com/testimonial/daniel-clifford"
+      >
+        <q
+          >I was an EMT for many years before I joined the bootcamp. I’ve been
+          looking to make a transition and have heard some people who had an
+          amazing experience here. I signed up for the free intro course and
+          found it incredibly fun! I enrolled shortly thereafter. The next 12
+          weeks was the best - and most grueling - time of my life. Since
+          completing the course, I’ve successfully switched careers, working as
+          a Software Engineer at a VR startup.</q
+        >
+      </blockquote>
+    </article>
+    <article class="card">
+      <header class="card__header">
+        <h1 class="card__title">Jonathan Walters</h1>
+        <p class="card__graduation">Verified Graduate</p>
+      </header>
+
+      <p class="card__description">
+        The team was very supportive and kept me motivated.
+      </p>
+
+      <blockquote
+        class="card__quote"
+        cite="https://example.com/testimonial/jonathan-walters"
+      >
+        <q
+          >I started as a total newbie with virtually no coding skills. I now
+          work as a mobile engineer for a big company. This was one of the best
+          investments I’ve made in myself.</q
+        >
+      </blockquote>
+    </article>
+    <article class="card">
+      <header class="card__header">
+        <h1 class="card__title">Jeanette Harmon</h1>
+        <p class="card__graduation">Verified Graduate</p>
+      </header>
+
+      <p class="card__description">
+        An overall wonderful and rewarding experience.
+      </p>
+
+      <blockquote
+        class="card__quote"
+        cite="https://example.com/testimonial/jeanette-harmon"
+      >
+        <q
+          >Thank you for the wonderful experience! I now have a job I really
+          enjoy, and make a good living while doing something I love.</q
+        >
+      </blockquote>
+    </article>
+    <article class="card">
+      <header class="card__header">
+        <h1 class="card__title">Patrick Abrams</h1>
+        <p class="card__graduation">Verified Graduate</p>
+      </header>
+
+      <p class="card__description">
+        The staff seem genuinely concerned about my progress which I find really
+        refreshing.
+      </p>
+
+      <blockquote
+        class="card__quote"
+        cite="https://example.com/testimonial/patrick-abrams"
+      >
+        <q
+          >I was an EMT for many years before I joined the bootcamp. I’ve been
+          looking to make a transition and have heard some people who had an
+          amazing experience here. I signed up for the free intro course and
+          found it incredibly fun! I enrolled shortly thereafter. The next 12
+          weeks was the best - and most grueling - time of my life. Since
+          completing the course, I’ve successfully switched careers, working as
+          a Software Engineer at a VR startup.</q
+        >
+      </blockquote>
+    </article>
+    <article class="card">
+      <header class="card__header">
+        <h1 class="card__title">Kira Whittle</h1>
+        <p class="card__graduation">Verified Graduate</p>
+      </header>
+
+      <p class="card__description">
+        Such a life-changing experience. Highly recommended!
+      </p>
+
+      <blockquote
+        class="card__quote"
+        cite="https://example.com/testimonial/kira-whittle"
+      >
+        <q
+          >Before joining the bootcamp, I’ve never written a line of code. I
+          needed some structure from professionals who can help me learn
+          programming step by step. I was encouraged to enroll by a former
+          student of theirs who can only say wonderful things about the program.
+          The entire curriculum and staff did not disappoint. They were very
+          hands-on and I never had to wait long for assistance. The agile team
+          project, in particular, was outstanding. It took my learning to the
+          next level in a way that no tutorial could ever have. In fact, I’ve
+          often referred to it during interviews as an example of my development
+          experience. It certainly helped me land a job as a full-stack
+          developer after receiving multiple offers. 100% recommend!</q
+        >
+      </blockquote>
+    </article>
+  </main>
 </template>
 
 <script>
