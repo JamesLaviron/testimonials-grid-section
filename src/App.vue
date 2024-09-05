d1877beaa21ce61ba396c6182b0f50384e42ee12
JamesLaviron <victor@gerfaud.fr>
2024-09-05 16:28:26 +0200

    Base project - cleaned

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+import "./assets/styles/main.scss";
+export default {
+  name: "App",
+  components: {},
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/testimonials-grid-section/" : "/",
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/styles/_reset.scss";
+          @import "@/assets/styles/base.scss";
+          @import "@/assets/styles/_variables.scss";
+          @import "@/assets/styles/_mixins.scss";
+          @import "@/assets/styles/text-presets.scss";
+        `
+      }
+    }
+  }
+})
